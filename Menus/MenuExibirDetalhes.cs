2423447301b29046902a20931fa150096608ddc3
KevenAbraham <abraham.keven20@gmail.com>
2023-12-30 01:06:11 -0300

    mudando as classes menus

--- Menus/MenuExibirDetalhes.cs
+++ Menus/MenuExibirDetalhes.cs
@@ -29,13 +29,4 @@ internal class MenuExibirDetalhes : Menu
             Console.Clear();
         }
     }
-    
-    public void ExibirTituloDaOpcao(string titulo)
-    {
-        int quantidadeDeLetras = titulo.Length;
-        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-        Console.WriteLine(asteriscos);
-        Console.WriteLine(titulo);
-        Console.WriteLine(asteriscos + "\n");
-    }
 }
