d723e727fdb9560d52828171d5457f31a307e5ce
KevenAbraham <abraham.keven20@gmail.com>
2023-12-30 00:53:08 -0300

    ajustando o program.cs

--- Program.cs
+++ Program.cs
@@ -1,73 +1,62 @@
 ﻿using ScreenSound.Menus;
 using ScreenSound.Modelos;
 
-internal class Program 
+Banda ira = new Banda("Ira!");
+ira.AdicionarNota(new Avaliacao(10));
+ira.AdicionarNota(new Avaliacao(8));
+ira.AdicionarNota(new Avaliacao(6));
+Banda beatles = new("The Beatles!");
+
+Dictionary<string, Banda> bandasRegistradas = new();
+bandasRegistradas.Add(ira.Nome, ira);
+bandasRegistradas.Add(beatles.Nome, beatles);
+
+Dictionary<int, Menu> op = new();
+op.Add(1, new MenuRegistrarBanda());
+op.Add(2, new MenuRegistrarAlbum());
+op.Add(3, new MenuMostrarBandasRegistradas());
+op.Add(4, new MenuAvaliarBanda());
+op.Add(5, new MenuExibirDetalhes());
+op.Add(-1, new MenuSair());
+
+void ExibirLogo()
 {
-    private static void Main(string[] args)
-    {
-        Banda ira = new Banda("Ira!");
-        ira.AdicionarNota(new Avaliacao(10));
-        ira.AdicionarNota(new Avaliacao(8));
-        ira.AdicionarNota(new Avaliacao(6));
-        Banda beatles = new("The Beatles!");
+    Console.WriteLine(@"
 
-
-        Dictionary<string, Banda> bandasRegistradas = new();
-        bandasRegistradas.Add(ira.Nome, ira);
-        bandasRegistradas.Add(beatles.Nome, beatles);
-
-        Dictionary<int, Menu> op = new();
-        op.Add(1, new MenuRegistrarBanda());
-        op.Add(2, new MenuRegistrarAlbum());
-        op.Add(3, new MenuMostrarBandasRegistradas());
-        op.Add(4, new MenuAvaliarBanda());
-        op.Add(5, new MenuExibirDetalhes());
-        op.Add(-1, new MenuSair());
-
-
-
-        void ExibirLogo()
-        {
-            Console.WriteLine(@"
-
-        ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-        ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-        ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-        ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-        ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-        ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
-        ");
-            Console.WriteLine("Boas vindas ao Screen Sound 2.0!");
-        }
-
-        void ExibirOpcoesDoMenu()
-        {
-            ExibirLogo();
-            Console.WriteLine("\nDigite 1 para registrar uma banda");
-            Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
-            Console.WriteLine("Digite 3 para mostrar todas as bandas");
-            Console.WriteLine("Digite 4 para avaliar uma banda");
-            Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
-            Console.WriteLine("Digite -1 para sair");
-
-            Console.Write("\nDigite a sua opção: ");
-            string opcaoEscolhida = Console.ReadLine()!;
-            int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-
-            if (op.ContainsKey(opcaoEscolhidaNumerica))
-            {
-                Menu menuASerExibido = op[opcaoEscolhidaNumerica];
-                menuASerExibido.Executar(bandasRegistradas);
-                if(opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
-            }
-            else
-            {
-                Console.WriteLine("Opção inválida");
-            }
-
-            
-    
-            ExibirOpcoesDoMenu();
-        }
-    }
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine("Boas vindas ao Screen Sound 2.0!");
 }
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
+    Console.WriteLine("Digite 3 para mostrar todas as bandas");
+    Console.WriteLine("Digite 4 para avaliar uma banda");
+    Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (op.ContainsKey(opcaoEscolhidaNumerica))
+    {
+        Menu menuASerExibido = op[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(bandasRegistradas);
+        if(opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida");
+    }    
+}
+
+ExibirOpcoesDoMenu();
